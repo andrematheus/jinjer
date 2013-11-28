2c7adc6fd0a53db44951eccb8f5db3b45e4a4653
Andre Roque Matheus <amatheus@mac.com>
2013-11-28 20:40:36 -0200

    Correct requires to force installation.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jinjer',
-    version='0.1',
+    version='0.2',
     packages=['jinjer'],
     package_dir={'': 'src'},
     url='https://github.com/andrematheus/jinjer',
@@ -10,7 +10,7 @@ setup(
     author='André Roque Matheus',
     author_email='amatheus@ligandoospontos.com.br',
     description='Tool to render Jinja templates from command line',
-    requires=['docopt', 'jinja2', 'PyYaml'],
+    install_requires=['docopt', 'jinja2', 'PyYaml'],
     entry_points={
         'console_scripts': [
             'jinjer = jinjer.jinjer:main'
